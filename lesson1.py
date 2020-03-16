93e985088fd042cfd7318e50690f5fdf24a90405
Shine <62229046+h13ns@users.noreply.github.com>
2020-03-16 20:54:55 +0800

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,113 @@
+# PYTHON 3 BASIC KAALAMAN
+
+print("Hello kapwa mag-aaral!")
+print(" Narito ako ngayon para magshare ng aking konting kaalaman tungkol sa basics ng Python")
+print("Handa ka na bang matuto? y/n")
+sagot = input()
+if sagot == "y" or "yes":
+    print("Ok simulan na natin!")
+else:
+    print("Edi wag che!")
+
+print("TOPIC 1: COMMENT")
+print("May dalawang klase ng python. Python 2 at 3.")
+print("Outdated na po ang 2 kaya 3 na ang gagamitin natin")
+print("Ang pinakabasic sa lahat para sakin ay ang comment")
+print("Ang isang statement ay lalagyan mo lang ng # para magiging comment ito.")
+print("Hindi ito nakikita sa compiler, sa text editor lamang.")
+print("Ang compiler ay isang program na kumuconvert ng codes")
+print("para maeexecute ng computer")
+print("Ang halimbawa ng compiler ay Pycharm")
+print("Ang Python ay hindi compiler, ito ay ang kinocompile")
+print("Kadalasan, ang comment ay ginagamit sa paglagay ng label o palatandaan")
+print("para di malito yung nagpoprogram")
+print("Ganito ang porma ng comment")
+print("# ito ay halimbawa ng isang comment")
+# ito ay halimbawa ng comment
+print("Kung di mo naiintindihan igoogle mo nalang. Bahala ka na sa buhay mo hehe")
+
+print("Handa ka na ba sa short quiz? y/n")
+sagot = input()
+if sagot == "y" or "yes":
+    print("Ok simulan na natin!")
+else:
+    print("Take your time hehe")
+    print("Pagready ka na, type y or yes")
+    print("sagot: ")
+    sagot = input()
+    if sagot == "y" or "yes":
+        print("Ok simulan na natin!")
+
+print("EXERCISE: Multiple choice. 5pts")
+
+print("1. Para saan nilalagay ang #?")
+print("A. Para sa label")
+print("B. Para sa comment")
+
+print("Sagot: ")
+sagot = input()
+
+if sagot == "B" or "b":
+    print("Natumbok mo! Ang # ay simbolo ng comment.")
+else:
+    print("Mali, subukang muli.")
+    print("Answer: ")
+    sagot = input()
+    if sagot == "B" or "b":
+        print("Sa wakas, natumbok mo! Ang # ay simbolo ng comment.")
+
+print("2. Saan banda nilalagay ang #?")
+print("A. sa dulo")
+print("B. sa gitna")
+print("C. sa una")
+
+print("Sagot: ")
+sagot = input()
+
+if sagot == "C" or "c":
+    print("Wow, mahusay! Kinakailangang nasa una ang # para maexecute ito")
+else:
+    print("Mali, subukang muli.")
+    print("Answer: ")
+    sagot = input()
+    if sagot == "C" or "c":
+        print("Sa wakas, natumbok mo! Kinakailangang nasa una ang # para maexecute ito")
+    else:
+        print("Mali, subukang muli.")
+        print("Answer: ")
+        sagot = input()
+        if sagot == "C" or "c":
+            print("Sa wakas, natumbok mo! Kinakailangang nasa una ang # para maexecute ito")
+
+print("3. Alin ang tamang statement tungkol sa comment?")
+print("A. Nakikita ito ng text editor pero hindi ito nababasa ng compiler")
+print("B. Nababasa ito ng compiler pero hindi ito nakikita ng text editor")
+print("C. Hindi ito nakikita ng text editor at compiler")
+print("D. Nakikita ito ng text editor at compiler")
+
+print("Sagot: ")
+sagot = input()
+
+if sagot == "A" or "a":
+    print("Wow mahusay! Ang comment ay nakikita lamang sa text editor. Hindi ito nakikita kung nirurun na ang program")
+else:
+    print("Mali, subukang muli.")
+    print("Sagot: ")
+    sagot = input()
+    if sagot == "A" or "a":
+         print("Wow mahusay! Ang comment ay nakikita lamang sa text editor. Hindi ito nakikita kung nirurun na ang program")
+    else:
+        print("Mali, subukang muli.")
+        print("Sagot: ")
+        sagot = input()
+        if sagot == "A" or "a":
+         print("Wow mahusay! Ang comment ay nakikita lamang sa text editor. Hindi ito nakikita kung nirurun na ang program")
+        else:
+            print("Mali, subukang muli. Ang comment ay nakikita lamang sa text editor. Hindi ito nakikita kung nirurun na ang program")
+            print("Sagot: ")
+            sagot = input()
+            if sagot == "A" or "a":
+                print("Wow mahusay! Ang comment ay nakikita lamang sa text editor. Hindi ito nakikita kung nirurun na ang program")
+
+print("At dahil maalam ka na sa comments,")
+print("Magmove on na tayo sa next lesson")
